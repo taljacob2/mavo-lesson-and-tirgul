eb234b44a728a0f0b31c60e705ebb2320fdcae27
Tal Jacob <taljacob2@gmail.com>
2022-11-11 09:21:01 +0200

    Create `toBinaryString` Function
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
+#include <limits.h>
 #include <stdio.h>
 
+#define FMT_BUF_SIZE (CHAR_BIT * sizeof(INT_MAX) + 1)
+
+/**
+ * @brief Converts a value to its binary representation, formatted as a string.
+ *
+ * For example:
+ * @code
+ * int value = 0x0A;
+ * printf("%s\n", binToStr(hex));
+ * // Outputs: 1010
+ * @endcode
+ *
+ * @param value Any value to be represented in binary, formatted as a string.
+ * @return The given `value`'s binary representation in a string format.
+ */
+char *toBinaryString(int value) {
+    char  buf[FMT_BUF_SIZE];
+    char *s = buf + FMT_BUF_SIZE;
+    *--s    = 0;
+    if (!value) *--s = '0';
+    for (; value; value /= 2) *--s = '0' + value % 2;
+    return s;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int hex = 0x0A;
+    printf("%s\n", toBinaryString(hex));
     return 0;
 }
