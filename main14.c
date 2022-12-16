796ba0cadb3134c3bcdd68f6718f59c6c6e9d769
taljacob2 <taljacob2@gmail.com>
2022-12-16 10:38:52 +0200

    Doc main14.c
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main14.c
+++ main14.c
@@ -3,12 +3,12 @@
 int main() {
 
     int              arr[]     = {1, 2};
-    int *            p         = arr;
-    const int *      constPInt = arr;
-    int *const       pIntConst = arr;
-    const int *const constIntP = arr;
+    int *            p         = arr; // "non-const" pointer, that points to "non-const" `int`.
+    const int *      constPInt = arr; // "non-const" pointer, that points to "const" `int`.
+    int *const       pIntConst = arr; // "const" pointer, that points to "non-const" `int`.
+    const int *const constIntP = arr; // "const" pointer, that points to "const" `int`.
 
-    //    printf("%d\n", (++arr)[0]); // Impossible because `arr` is `const`.
+    //    printf("%d\n", (++arr)[0]);       // Impossible, because `arr` is `int * const`.
     //    printf("%d\n", (++p)[0]);         // Possible
     //    printf("%d\n", (++constPInt)[0]); // Possible
     //    printf("%d\n", (++pIntConst)[0]); // Impossible
