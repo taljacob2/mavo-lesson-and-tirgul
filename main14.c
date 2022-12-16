cf87b1163bf2a0cdbbffda584146611423debb49
taljacob2 <taljacob2@gmail.com>
2022-12-16 10:50:42 +0200

    Rename Variables In main14.c
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main14.c
+++ main14.c
@@ -2,17 +2,17 @@
 
 int main() {
 
-    int              arr[]     = {1, 2};
-    int *            p         = arr; // "non-const" pointer, that points to "non-const" `int`.
-    const int *      constPInt = arr; // "non-const" pointer, that points to "const" `int`.
-    int *const       pIntConst = arr; // "const" pointer, that points to "non-const" `int`.
-    const int *const constIntP = arr; // "const" pointer, that points to "const" `int`.
+    int              arr[]          = {1, 2};
+    int *            intP           = arr; // "non-const" pointer, that points to "non-const" `int`.
+    const int *      constIntP      = arr; // "non-const" pointer, that points to "const" `int`.
+    int *const       intPConst      = arr; // "const" pointer, that points to "non-const" `int`.
+    const int *const constIntPConst = arr; // "const" pointer, that points to "const" `int`.
 
-    //    printf("%d\n", (++arr)[0]);       // Impossible, because `arr` is `int * const`.
-    //    printf("%d\n", (++p)[0]);         // Possible
-    //    printf("%d\n", (++constPInt)[0]); // Possible
-    //    printf("%d\n", (++pIntConst)[0]); // Impossible
-    //    printf("%d\n", (++constIntP)[0]); // Impossible
+    //    printf("%d\n", (++arr)[0]);            // Impossible, because `arr` is `int * const`.
+    //    printf("%d\n", (++intP)[0]);           // Possible
+    //    printf("%d\n", (++constIntP)[0]);      // Possible
+    //    printf("%d\n", (++intPConst)[0]);      // Impossible
+    //    printf("%d\n", (++constIntPConst)[0]); // Impossible
 
     return 0;
 }
