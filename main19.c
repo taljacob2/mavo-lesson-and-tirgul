b76c6cdb2100406c98d4568a02304614b8505f21
taljacob2 <taljacob2@gmail.com>
2022-12-25 11:22:19 +0200

    main19.c
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

---
+++ main19.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void skipCharacterWhilePrintingDynamicallyAllocating(const char *string,
+                                                     char        charToSkip) {
+    char *newString = malloc(strlen(string) + sizeof(char) * 1);
+    while (*string) {
+        if (*string != charToSkip) {
+            *newString = *string;
+        }
+        string++;
+    }
+
+    // Concise the memory allocation.
+    newString = realloc(newString, strlen(string) + sizeof(char) * 1);
+
+    puts(newString); // Do something.
+
+    free(newString);
+}
+
+
+void skipCharacterWhilePrinting(const char *string, char charToSkip) {
+    while (*string) {
+        if (*string != charToSkip) { putchar(*string); }
+        string++;
+    }
+}
+
+void skipECharacterWhilePrinting(const char *string) {
+    skipCharacterWhilePrinting(string, 'e');
+}
+
+int main() {
+    skipECharacterWhilePrinting("Hello This Is Me.");
+    return 0;
+}
