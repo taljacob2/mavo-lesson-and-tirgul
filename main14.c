895c1266279d65c9fbf33f3e7fcabb02aac7b444
Tal Jacob <taljacob2@gmail.com>
2022-12-31 14:03:56 +0200

    Add Doc To main14.c
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- main14.c
+++ main14.c
@@ -2,6 +2,16 @@
 
 int main() {
 
+    /*
+     * The "const" keywords means you cannot change the value of the variable.
+     *
+     * - In case it is a `const int` it means you cannot change the value of the
+     *   `int`.
+     * - In case it is a `int * const` (a.k.a "const pointer"), it means that
+     *   you cannot change the pointer to point to another `int`.
+     *   And it must point to the initial `int` you configured it to point to.
+     */
+
     int              arr[]          = {1, 2};
     int *            intP           = arr; // "non-const" pointer, that points to "non-const" `int`.
     const int *      constIntP      = arr; // "non-const" pointer, that points to "const" `int`.
