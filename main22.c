f180cf420aa5445680bce9f06576acb41c99ab5f
taljacob2 <taljacob2@gmail.com>
2023-01-06 08:34:19 +0200

    Add An Important Comment For Copying A Str Via The `=` Operator
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main22.c
+++ main22.c
@@ -46,5 +46,19 @@ int main() {
     printf("struct1.names = %s\n", struct1.names);
     printf("struct1_2.names = %s\n", struct1_2.names);
 
+
+    /*
+     * **IMPORTANT:**
+     *
+     * As you can see, `struct1.names` and `struct1_2.names` are independent of
+     * each other. Because they were **allocated statically**.
+     *
+     * In case we were allocating them dynamically (via `malloc`), then the copy
+     * would have been getting `struct1_2.names` to point to the same `char *`
+     * that was allocated at `struct1.names`. And thus changing
+     * `struct1_2.names` would have caused a change in `struct1.names`.
+     * In this case, we should have make a **manual** deep copy for `names`.
+     */
+
     return 0;
 }
