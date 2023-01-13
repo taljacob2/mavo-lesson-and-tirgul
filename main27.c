507ea6106176efaa7edebdd1ab4b014cb3f2e75a
taljacob2 <taljacob2@gmail.com>
2023-01-13 09:17:38 +0200

    Add Yehuda Implementation
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main27.c
+++ main27.c
@@ -17,6 +17,20 @@ int findMaxElement(int *arr, int size) {
     return maxElement;
 }
 
+/**
+ * @author Yehuda Elmaliahi
+ */
+int findMaxElementYehuda(int *arr, int size) {
+    if (size == 1) { return arr[0]; }
+
+    int returnValueOfNextFindMaxElement =
+            findMaxElementYehuda(arr + 1, size - 1);
+    int maxElement = arr[0] > returnValueOfNextFindMaxElement
+                             ? arr[0]
+                             : returnValueOfNextFindMaxElement;
+    return maxElement;
+}
+
 int main() {
     int arr[SIZE] = {0, 1, 2, 3, 4, 9, 6, 7, 8, 5};
     printf("findMaxElement = %d\n", findMaxElement(arr, SIZE));
