411694f640a5341a45662160caaa821a2220e072
taljacob2 <taljacob2@gmail.com>
2023-01-13 11:17:21 +0200

    Init main34.c 2
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main34.c
+++ main34.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define M 3
-#define N 5
+#define M 7
+#define N 9
 
 /*
  * Given a matrix with size [M][N].
@@ -11,13 +11,26 @@
  * element which is larger than the current element.
  */
 
-int findPath(int i, int j, int mat[][N]){
+/**
+ *
+ * @param i
+ * @param j
+ * @param mat
+ * @return
+ *
+ * @author Yehuda Elmaliahi
+ */
+int findPath(int i, int j, int mat[][N], int maxRow) {
 
 }
 
 int main() {
-    int matr[M][N] = {{1, 2, 3, 4, 5},
-                      {1, 2, 3, 4, 5},
-                      {1, 2, 3, 4, 5}};
+    int matr[M][N] = {{7, 3, 2, 18, 20, 25, 28, 30, 34},
+                      {3, 2, 1, 9, 13, -4, 18, 11, 39},
+                      {6, -7, 3, -3, 11, 12, -9, 21, 44},
+                      {8, 5, 7, 8, 10, 19, 11, 55, 49},
+                      {10, 3, -2, 88, -9, -7, 60, 59, 31},
+                      {20, 95, 85, 83, 80, 43, 65, 69},
+                      {30, -9, 21, 89, 5, 75, 72, 70, 34}};
     return 0;
 }
