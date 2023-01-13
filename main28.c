57b891ff23d53e6f0b17eeace9d4ce6b3c3dfc4f
taljacob2 <taljacob2@gmail.com>
2023-01-13 09:58:10 +0200

    Add findElementByBinarySearchYehuda Patched By Tal Jacob
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main28.c
+++ main28.c
@@ -5,33 +5,42 @@
 #define ERROR INT_MIN
 
 /**
- * @author Tal Jacob
- */
-int findMaxElement(int *arr, int size) {
-    if (size <= 0) { return ERROR; }
-
-    int returnValueOfNextFindMaxElement = findMaxElement(arr + 1, size - 1);
-    int maxElement = arr[0] > returnValueOfNextFindMaxElement
-                             ? arr[0]
-                             : returnValueOfNextFindMaxElement;
-    return maxElement;
-}
-
-/**
  * @attention unsupported when `size` is "less than or equal" to `0`.
  *
  * @attention Given the `arr` is already sorted!!!
  *
  * @return In case the `element` is found, returns the index of the `element`.
  *         Else returns `-1`.
+ * @author Tal Jacob.
+ */
+int findElementByBinarySearch(int *arr, int size, int element) {
+    if (size <= 0) { return -1; }
+}
+
+
+/**
+ * @attention unsupported when `size` is "less than or equal" to `0`.
+ *
+ * @attention Given the `arr` is already sorted!!!
+ *
  * @author Yehuda Elmaliahi / Patched by Tal Jacob.
  */
-int findElementByBinarySearchYehuda(int *arr, int size, int element) {
+int *findElementByBinarySearchYehuda(int *arr, int size, int element) {
+    if (size == 0) { return NULL; }
 
+    if (arr[size / 2] == element) {
+        return arr + (size / 2);
+    } else if (arr[size / 2] < element) {
+        return findElementByBinarySearchYehuda(
+                arr + (size / 2), (size / 2) + (size % 2), element);
+    } else {
+        return findElementByBinarySearchYehuda(arr, size / 2, element);
+    }
 }
 
 int main() {
     int arr[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    printf("findMaxElement = %d\n", findElementByBinarySearchYehuda(arr, SIZE));
+    printf("findElementByBinarySearchYehuda = %d\n",
+           *findElementByBinarySearchYehuda(arr, SIZE, 0));
     return 0;
 }
