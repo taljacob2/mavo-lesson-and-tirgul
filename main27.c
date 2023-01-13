e62a66aac74bb358ec5b5eb85fafac7240392c9b
taljacob2 <taljacob2@gmail.com>
2023-01-13 09:11:49 +0200

    Add `main27.c`
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

---
+++ main27.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include <stdio.h>
+
+#define SIZE  10
+#define ERROR INT_MIN
+
+int findMaxElement(int *arr, int size) {
+    if (size == 0) { return ERROR; }
+
+    int returnValueOfNextFindMaxElement = findMaxElement(arr + 1, size - 1);
+    int maxElement = arr[0] > returnValueOfNextFindMaxElement
+                             ? arr[0]
+                             : returnValueOfNextFindMaxElement;
+    return maxElement;
+}
+
+int main() {
+    int arr[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printf("findMaxElement = %d\n", findMaxElement(arr, SIZE));
+    return 0;
+}
