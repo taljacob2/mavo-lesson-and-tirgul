77920bdb17c1b1addb04b8b2bbada63a11234ccd
taljacob2 <taljacob2@gmail.com>
2023-01-13 09:03:04 +0200

    Add `sumArrReversed` To `main26.c`
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main26.c
+++ main26.c
@@ -7,8 +7,14 @@ int sumArr(int *arr, int size) {
     return sumArr(arr + 1, size - 1) + *arr;
 }
 
+int sumArrReversed(int *arr, int size) {
+    if (size == 0) { return 0; }
+    return sumArr(arr, size - 1) + arr[size - 1];
+}
+
 int main() {
     int arr[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     printf("sumArr = %d\n", sumArr(arr, SIZE));
+    printf("sumArrReversed = %d\n", sumArrReversed(arr, SIZE));
     return 0;
 }
