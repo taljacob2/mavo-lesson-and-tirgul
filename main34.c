0220a669700911378fd4253213d16f6be7a28eb6
taljacob2 <taljacob2@gmail.com>
2023-01-13 11:13:06 +0200

    Init main34.c
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

---
+++ main34.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define M 3
+#define N 5
+
+/*
+ * Given a matrix with size [M][N].
+ * every element has at most 4 neighbor elements.
+ * You need to print the longest path (i.e. sequence) of elements,
+ * where you start from a given element, and you can only pass through an
+ * element which is larger than the current element.
+ */
+
+int findPath(int i, int j, int mat[][N]){
+
+}
+
+int main() {
+    int matr[M][N] = {{1, 2, 3, 4, 5},
+                      {1, 2, 3, 4, 5},
+                      {1, 2, 3, 4, 5}};
+    return 0;
+}
