41f27cdac7a4ab69367268848bef6c417705a3ae
Tal Jacob <taljacob2@gmail.com>
2023-01-15 17:14:22 +0200

    Edit `main32.c`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- main32.c
+++ main32.c
@@ -19,28 +19,10 @@ void flipYehuda(char *s, int size) {
     flipYehuda(s + 1, size - 2);
 }
 
-/**
- *
- * @param string
- * @author Tal Jacob
- */
-
-// TODO: need to fix
-void flip(char *string) {
-    if (!string) { return; }
-    if (strlen(string) == 1) { return; }
-
-    char temp                      = *string;
-    *string                        = *(string + strlen(string) - 1);
-    *(string + strlen(string) - 1) = temp;
-
-    flip(string + 1);
-}
-
 int main() {
 
     char string[6] = "Hello";
-    flip(string);
+    flipYehuda(string, strlen(string));
     printf("%s\n", string);
 
     return 0;
