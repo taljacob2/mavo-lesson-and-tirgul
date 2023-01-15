060ba876f157ea6ca8212a47faa2fd2e3e4e0d2e
Tal Jacob <taljacob2@gmail.com>
2023-01-15 17:25:18 +0200

    Add Question To `main34.c`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- main34.c
+++ main34.c
@@ -9,15 +9,12 @@
  * You need to print the longest path (i.e. sequence) of elements,
  * where you start from a given element, and you can only pass through an
  * element which is larger than the current element.
+ *
+ * see question here:
+ * https://user-images.githubusercontent.com/70590583/212549974-e5ab8944-ad48-4853-9cce-94acb9021a39.jpg
  */
 
 /**
- *
- * @param i
- * @param j
- * @param mat
- * @return
- *
  * @author Yehuda Elmaliahi
  */
 int findPath(int i, int j, int mat[][N], int maxRow) {
@@ -31,20 +28,22 @@ int findPath(int i, int j, int mat[][N], int maxRow) {
     }
 
     // check down
+    // continue implementing here...
 
     // check right
+    // continue implementing here...
 
     // check left
-
+    // continue implementing here...
 }
 
 int main() {
-    int matr[M][N] = {{7, 3, 2, 18, 20, 25, 28, 30, 34},
-                      {3, 2, 1, 9, 13, -4, 18, 11, 39},
-                      {6, -7, 3, -3, 11, 12, -9, 21, 44},
-                      {8, 5, 7, 8, 10, 19, 11, 55, 49},
-                      {10, 3, -2, 88, -9, -7, 60, 59, 31},
-                      {20, 95, 85, 83, 80, 43, 65, 69},
-                      {30, -9, 21, 89, 5, 75, 72, 70, 34}};
+    int mat[M][N] = {{7, 3, 2, 18, 20, 25, 28, 30, 34},
+                     {3, 2, 1, 9, 13, -4, 18, 11, 39},
+                     {6, -7, 3, -3, 11, 12, -9, 21, 44},
+                     {8, 5, 7, 8, 10, 19, 11, 55, 49},
+                     {10, 3, -2, 88, -9, -7, 60, 59, 31},
+                     {20, 95, 85, 83, 80, 43, 65, 69},
+                     {30, -9, 21, 89, 5, 75, 72, 70, 34}};
     return 0;
 }
