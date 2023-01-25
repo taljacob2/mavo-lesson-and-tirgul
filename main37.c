17a15eb5acc3e09375547f28c7b968f255450fce
taljacob2 <taljacob2@gmail.com>
2023-01-25 20:43:44 +0200

    My solution to main37.c
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

---
+++ main37.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define FALSE 0
+#define TRUE  1
+typedef int BOOLEAN;
+
+
+BOOLEAN isPalindrome(char *strStartPosition, char *strEndPosition) {
+    char *savedStrStartPosition = strStartPosition;
+    char *savedStrEndPosition   = strEndPosition;
+    while (*strStartPosition == *strEndPosition) {
+        strStartPosition++;
+        strEndPosition--;
+        if (strStartPosition == savedStrEndPosition &&
+            strEndPosition == savedStrStartPosition) {
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+int findPalindromeLengthFromPosition(char *strStartPosition) {
+    char *saveStartPosition = strStartPosition;
+    char  startChar         = *strStartPosition++;
+    while (*strStartPosition) {
+        if (*strStartPosition == startChar) {
+            if (isPalindrome(saveStartPosition, strStartPosition)) {
+                return strStartPosition - saveStartPosition + 1;
+            }
+        }
+        strStartPosition++;
+    }
+
+    return 1; // The trivial palindrome with the length of `1` .
+}
+
+int longestPalindromeSubstring(char *str) {
+    int maxLength = 0;
+    while (*str) {
+        int longestLengthSinceThisPosition =
+                findPalindromeLengthFromPosition(str);
+        if (longestLengthSinceThisPosition > maxLength) {
+            maxLength = longestLengthSinceThisPosition;
+        }
+        str++;
+    }
+
+    return maxLength;
+}
+
+int main() {
+    printf("%d\n", longestPalindromeSubstring("toyota"));
+
+    return 0;
+}
