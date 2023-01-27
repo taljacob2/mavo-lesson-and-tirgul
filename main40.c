a86bd7cafbece6756c7902c18b68b1c212269e4f
taljacob2 <taljacob2@gmail.com>
2023-01-27 09:08:40 +0200

    Init bubbleSort Definition
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main40.c
+++ main40.c
@@ -2,12 +2,16 @@
 
 #define N 5
 
+void bubbleSort(int *arr, size_t size){
+
+}
+
 /**
  * - If rows has even index (e.g. 0, 2, 4...), then sort by ascending order.
  * - If rows has odd index (e.g. 1, 3, 5...), then sort by descending order.
  * @param numRows
  */
-void crossSort(mat[][N], int numRows){
+void crossSort(int mat[][N], int numRows){
 
 }
 
