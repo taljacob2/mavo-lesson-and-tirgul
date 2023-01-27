a8fd45aa74b6b6d3255a5bb05f034a6696d82dd6
taljacob2 <taljacob2@gmail.com>
2023-01-27 09:05:18 +0200

    Init main40.c
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

---
+++ main40.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define N 5
+
+/**
+ * - If rows has even index (e.g. 0, 2, 4...), then sort by ascending order.
+ * - If rows has odd index (e.g. 1, 3, 5...), then sort by descending order.
+ * @param numRows
+ */
+void crossSort(mat[][N], int numRows){
+
+}
+
+int main() { return 0; }
