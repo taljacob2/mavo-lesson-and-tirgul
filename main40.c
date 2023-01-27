4a8b99e2937cf5199307148abd6ca9351ad41f3f
taljacob2 <taljacob2@gmail.com>
2023-01-27 09:15:51 +0200

    Add TODO Comments For `main40.c`
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main40.c
+++ main40.c
@@ -23,13 +23,16 @@ void bubbleSort(int *arr, size_t size, BOOLEAN (*compare)(int, int)){
     }
 }
 
+// TODO: need to implement compare upwards.
+// TODO: need to implement compare downwards.
+
 /**
  * - If rows has even index (e.g. 0, 2, 4...), then sort by ascending order.
  * - If rows has odd index (e.g. 1, 3, 5...), then sort by descending order.
  * @param numRows
  */
 void crossSort(int mat[][N], int numRows){
-
+    // TODO: need to implement.
 }
 
 int main() { return 0; }
