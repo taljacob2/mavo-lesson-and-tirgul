15a7d219c803c381b605914be8caa0fddc46387a
taljacob2 <taljacob2@gmail.com>
2023-01-27 11:06:34 +0200

    Remove Redundant Import
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- main43.c
+++ main43.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define FALSE 0
 #define TRUE  1
 typedef int BOOLEAN;
+
 
 int isNeededToAddToSum(int sumSoFar, int nextNum) {
     if ((sumSoFar >= 0) && (sumSoFar + nextNum) < 0) { return FALSE; }
